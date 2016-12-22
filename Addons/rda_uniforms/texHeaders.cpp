c5a70123019b352d5acb20da3bdf9a3647ad91a2
TigerDK <simontkristensen@hotmail.com>
2016-12-22 21:29:57 +0100

    Add files via upload

---
+++ Addons/rda_uniforms/texHeaders.cpp
@@ -0,0 +1,116 @@
+==================data\DanishM01Des\dkDesM01Cloth.paa==============
+TextureType 6 (DXT1)
+ARGBColor 1.000000,0.349020,0.321569,0.247059
+AVGCTAGG FF59523F
+MAXCTAGG FFFFFFFF
+boolBytes 00000001
+SomeCount 00000000
+nMips 11
+MipBox0: 4096x4096@offset 00000070
+MipBox1: 2048x2048@offset 005EEF2E
+MipBox2: 1024x1024@offset 00783393
+MipBox3: 512x512@offset 007ED3ED
+MipBox4: 256x256@offset 00808E78
+MipBox5: 128x128@offset 008100C7
+MipBox6: 64x64@offset 008120CE
+MipBox7: 32x32@offset 008128D5
+MipBox8: 16x16@offset 00812ADC
+MipBox9: 8x8@offset 00812B63
+MipBox10: 4x4@offset 00812B8A
+DummyMip @offset 00812B9F
+==================data\DanishM01Des\dkDesM01Vests.paa==============
+TextureType 6 (DXT1)
+ARGBColor 1.000000,0.309804,0.282353,0.200000
+AVGCTAGG FF4F4833
+MAXCTAGG FFFFFFFF
+boolBytes 00000001
+SomeCount 00000000
+nMips 10
+MipBox0: 2048x2048@offset 00000070
+MipBox1: 1024x1024@offset 001A56AF
+MipBox2: 512x512@offset 00211CEB
+MipBox3: 256x256@offset 0022D862
+MipBox4: 128x128@offset 00234B12
+MipBox5: 64x64@offset 00236B19
+MipBox6: 32x32@offset 00237320
+MipBox7: 16x16@offset 00237527
+MipBox8: 8x8@offset 002375AE
+MipBox9: 4x4@offset 002375D5
+DummyMip @offset 002375EA
+==================data\DanishM01Des\dkDesM01Equip.paa==============
+TextureType 6 (DXT1)
+ARGBColor 1.000000,0.305882,0.278431,0.211765
+AVGCTAGG FF4E4736
+MAXCTAGG FFFFFFFF
+boolBytes 00000001
+SomeCount 00000000
+nMips 10
+MipBox0: 2048x2048@offset 00000070
+MipBox1: 1024x1024@offset 00163492
+MipBox2: 512x512@offset 001C3723
+MipBox3: 256x256@offset 001DD201
+MipBox4: 128x128@offset 001E3EC7
+MipBox5: 64x64@offset 001E5ECE
+MipBox6: 32x32@offset 001E66D5
+MipBox7: 16x16@offset 001E68DC
+MipBox8: 8x8@offset 001E6963
+MipBox9: 4x4@offset 001E698A
+DummyMip @offset 001E699F
+==================data\DanishMTP\dkMTPM11cloth.paa==============
+TextureType 6 (DXT1)
+ARGBColor 1.000000,0.211765,0.211765,0.164706
+AVGCTAGG FF36362A
+MAXCTAGG FFFFFFFF
+boolBytes 00000001
+SomeCount 00000000
+nMips 11
+MipBox0: 4096x4096@offset 00000070
+MipBox1: 2048x2048@offset 0062C2C0
+MipBox2: 1024x1024@offset 007D1EF9
+MipBox3: 512x512@offset 00841F4F
+MipBox4: 256x256@offset 0085F07D
+MipBox5: 128x128@offset 00866815
+MipBox6: 64x64@offset 0086881C
+MipBox7: 32x32@offset 00869023
+MipBox8: 16x16@offset 0086922A
+MipBox9: 8x8@offset 008692B1
+MipBox10: 4x4@offset 008692D8
+DummyMip @offset 008692ED
+==================data\DanishMTP\dkMTPM11vests.paa==============
+TextureType 6 (DXT1)
+ARGBColor 1.000000,0.258824,0.250980,0.211765
+AVGCTAGG FF424036
+MAXCTAGG FFFFFFFF
+boolBytes 00000001
+SomeCount 00000000
+nMips 10
+MipBox0: 2048x2048@offset 00000070
+MipBox1: 1024x1024@offset 001C7FA3
+MipBox2: 512x512@offset 0023A42B
+MipBox3: 256x256@offset 002576CB
+MipBox4: 128x128@offset 0025EEAF
+MipBox5: 64x64@offset 00260EB6
+MipBox6: 32x32@offset 002616BD
+MipBox7: 16x16@offset 002618C4
+MipBox8: 8x8@offset 0026194B
+MipBox9: 4x4@offset 00261972
+DummyMip @offset 00261987
+==================data\DanishMTP\dkMTPM11equip.paa==============
+TextureType 6 (DXT1)
+ARGBColor 1.000000,0.254902,0.250980,0.219608
+AVGCTAGG FF414038
+MAXCTAGG FFFFFFFF
+boolBytes 00000001
+SomeCount 00000000
+nMips 10
+MipBox0: 2048x2048@offset 00000070
+MipBox1: 1024x1024@offset 0017EA87
+MipBox2: 512x512@offset 001E2CDE
+MipBox3: 256x256@offset 001FD648
+MipBox4: 128x128@offset 002046D7
+MipBox5: 64x64@offset 002066DE
+MipBox6: 32x32@offset 00206EE5
+MipBox7: 16x16@offset 002070EC
+MipBox8: 8x8@offset 00207173
+MipBox9: 4x4@offset 0020719A
+DummyMip @offset 002071AF
